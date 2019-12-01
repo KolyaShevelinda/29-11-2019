2f211053035c0781dd3b942d847c61e6a1ca6e3a
KolyaShevelinda <kolyashevelinda@gmail.com>
2019-12-02 01:04:20 +0200

    create two buttons with functional

--- js/index.js
+++ js/index.js
@@ -16,16 +16,22 @@ function clickHandler(event) {
     inputElement.setAttribute('type', type);
     parentElement.appendChild(inputElement);
     inputElement.value = targetElement.innerText;
+    inputElement.classList.add('form-control');
+
+    let buttonOk = createButton('btn-primary', 'glyphicon-ok', inputElement, targetElement);
+    let buttonCancel = createButton('btn-danger', 'glyphicon-remove', inputElement, targetElement);
+    parentElement.appendChild(buttonOk);
+    parentElement.appendChild(buttonCancel);
 
     parentElement.removeChild(targetElement);
 
     // inputElement.focus();
     inputElement.select();
 
-    inputElement.addEventListener('blur', function (event) {
-        parentElement.appendChild(targetElement);
-        parentElement.removeChild(inputElement);
-    });
+    // inputElement.addEventListener('blur', function (event) {
+    //     parentElement.appendChild(targetElement);
+    //     parentElement.removeChild(inputElement);
+    // });
 
     inputElement.addEventListener('keyup', function (event) {
         // console.log(event);
@@ -42,3 +48,39 @@ function clickHandler(event) {
         }
     });
 } 
+
+function createIcon(iconName) {
+    let iconElement = document.createElement('span');
+    iconElement.setAttribute('aria-hidden', true);
+    let iconClasses = ["glyphicon", iconName];
+    iconElement.classList.add(...iconClasses);
+    return iconElement;   
+}
+
+function createButton(btnColorClass, iconName, inputElement, targetElement) {
+    let btnElement = document.createElement('button');
+    btnElement.setAttribute('type', 'button');
+    let btnClasses = ["btn", btnColorClass];
+    btnElement.classList.add(...btnClasses); 
+
+    let spanEl = createIcon(iconName);
+    btnElement.appendChild(spanEl);
+
+    btnElement.addEventListener('click', function(event) {
+
+        if (event.target.classList[1] === 'glyphicon-ok') {
+            targetElement.innerText = inputElement.value;
+            inputElement.parentNode.appendChild(targetElement);
+
+        } else {
+            inputElement.parentNode.appendChild(targetElement);
+        }
+        let buttons = inputElement.parentNode.getElementsByClassName("btn");
+        [...buttons].forEach(function(btn) {
+            btn.remove();
+        })
+        inputElement.parentNode.removeChild(inputElement);
+    });
+
+    return btnElement;
+}
